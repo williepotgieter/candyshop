d25bca5beee236f744877ef17da0f65760c19626
williepotgieter <willie.potgieter@kurtosys.com>
2021-03-02 23:35:59 +0200

    Moved "GetAllCandyNames" function to reading.go file.

--- pkg/storage/storage-postgres.go
+++ pkg/storage/storage-postgres.go
@@ -34,30 +34,3 @@ func SetupStorage() (*Storage, error) {
 
 	return &Storage{db: conn}, nil
 }
-
-func (s *Storage) GetAllCandyNames() ([]Candy, error) {
-	var candy Candy
-	var candies []Candy
-
-	rows, err := s.db.Query("SELECT * FROM candies;")
-	if err != nil {
-		return []Candy{}, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&candy.ID, &candy.Created, &candy.Modified, &candy.Category, &candy.Name, &candy.Price)
-		if err != nil {
-			return []Candy{}, err
-		}
-
-		candies = append(candies, candy)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return []Candy{}, err
-	}
-
-	return candies, nil
-}
